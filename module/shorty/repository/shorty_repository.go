b0cb4fa0bfb7e3734db4951b21b7e888dc155665
feildrixliemdra <feildrix.liemdra@ralali.com>
2019-11-30 20:57:37 +0700

    add service and repository interface

--- module/shorty/repository/shorty_repository.go
+++ module/shorty/repository/shorty_repository.go
@@ -1,1 +1,10 @@
 package repository
+
+import (
+	"github.com/feildrixliemdra/shorty-hexagonal/module/shorty/model"
+)
+
+type ShortyRepositoryInterface interface {
+	FindByShortCode(shorten_url string) (*model.Shorty, error)
+	Store(shortyModel model.Shorty) (model.Shorty, error)
+}
--- module/shorty/service/shorty_service.go
+++ module/shorty/service/shorty_service.go
@@ -1,1 +1,50 @@
 package service
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/feildrixliemdra/shorty-hexagonal/module/shorty/model"
+	shortyRepository "github.com/feildrixliemdra/shorty-hexagonal/module/shorty/repository"
+	errs "github.com/pkg/errors"
+	"github.com/teris-io/shortid"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"gopkg.in/dealancer/validate.v2"
+)
+
+var (
+	ErrRedirectNotFound = errors.New("Shorten url Not Found")
+	ErrRedirectInvalid  = errors.New("Shorten url Invalid")
+)
+
+type ShortyServiceInterface interface {
+	FindByShortCode(shorten_url string) (*model.Shorty, error)
+	Store(redirect model.Shorty) (model.Shorty, error)
+}
+
+type ShortyService struct {
+	repository shortyRepository.ShortyRepositoryInterface
+}
+
+func NewShortyService(repo shortyRepository.ShortyRepositoryInterface) ShortyServiceInterface {
+	return &ShortyService{
+		repo,
+	}
+}
+
+func (service *ShortyService) FindByShortCode(shorten_url string) (*model.Shorty, error) {
+	return service.repository.FindByShortCode(shorten_url)
+}
+
+func (service *ShortyService) Store(shortyModel model.Shorty) (model.Shorty, error) {
+	fmt.Println(shortyModel.Url)
+	if err := validate.Validate(shortyModel.Url); err != nil {
+		fmt.Println("error: ", err.Error())
+		return model.Shorty{}, errs.Wrap(ErrRedirectInvalid, "service.Shorty.Store")
+	}
+	shortyModel.ID = primitive.NewObjectID()
+	shortyModel.ShortenUrl = shortid.MustGenerate()
+	shortyModel.CreatedAt = time.Now()
+	return service.repository.Store(shortyModel)
+}
